fbfd95da9c6c9241d8b40decf6fede485557fb7e
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-11 18:32:57 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,12 @@
+info = {
+        "name" : "AzamShaikh",
+        "sub" : ["python","java","c"],
+        "topic" : ("dict", "set"),
+        "age" : 21,
+        "isAdult" : True,
+        12.99 : 94.4
+}
+print(info)
+print(info["name"])
+print(info["sub"])
+print(info["age"])
