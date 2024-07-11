2047ecc8e76f1a9e3016f4d70f6fc3f80547f06a
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-11 18:39:55 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,12 @@
+marks = {} 
+
+x=int(input("enter phy : "))
+marks.update({"phy" : x})
+
+x = int(input("enter che: "))
+marks.update({"che" : x})
+
+x = int(input("enter eng: "))
+marks.update({"eng" : x})
+
+print(marks)
