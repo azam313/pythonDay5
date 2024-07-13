8f6458692f0327d8f12d51dfbaa9e36cea5f6c84
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:26:45 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,7 @@
+def sum_two_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    sum = num1 + num2
+    print("The sum of the two numbers is:", sum)
+
+sum_two_numbers()
