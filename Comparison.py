37b676fc04d0f37bbec2a458c3003c71066a35b9
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:29:41 +0530

    Create Comparison.py

---
+++ Comparison.py
@@ -0,0 +1,9 @@
+def compare_numbers():
+    a = float(input("Enter the first number (a): "))
+    b = float(input("Enter the second number (b): "))
+    if a >= b:
+        print("True")
+    else:
+        print("False")
+
+compare_numbers()
