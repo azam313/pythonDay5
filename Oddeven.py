14a1412857e6fab38988025f5b6d6d0ed3459907
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:30:21 +0530

    Create Oddeven.py

---
+++ Oddeven.py
@@ -0,0 +1,8 @@
+def check_even_odd():
+    number = int(input("Enter an integer: "))
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+check_even_odd()
