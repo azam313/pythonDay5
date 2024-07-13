c0ceec29b4cc6a92b7afaef0e7e81a3072b9e73c
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:31:43 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_series():
+    n = int(input("Enter the number of terms: "))
+    a, b = 0, 1
+    print("Fibonacci Series:")
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+fibonacci_series()
