0a945e82fde4ae7c26c5416f0f7a9d0aa0096c9e
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:28:27 +0530

    Create Average.py

---
+++ Average.py
@@ -0,0 +1,7 @@
+def calculate_average():
+    num1 = float(input("Enter the first floating-point number: "))
+    num2 = float(input("Enter the second floating-point number: "))
+    average = (num1 + num2) / 2
+    print("The average of the two numbers is:", average)
+
+calculate_average()
