a96ab371177fd599af8c1a36d836134340080d11
Azam Shaikh <80144479+azam313@users.noreply.github.com>
2024-07-13 18:27:41 +0530

    Create Asqure.py

---
+++ Asqure.py
@@ -0,0 +1,6 @@
+def calculate_square_area():
+    side_length = float(input("Enter the side length of the square: "))
+    area = side_length ** 2
+    print("The area of the square is:", area)
+
+calculate_square_area()
